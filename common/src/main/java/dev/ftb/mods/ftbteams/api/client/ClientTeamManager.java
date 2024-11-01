5794e14aaddab6d036797eeccd2180641493891b
Lonmo0208 <95102577+Lonmo0208@users.noreply.github.com>
2024-11-01 23:27:30 +0800

    Update ClientTeamManager.java

--- common/src/main/java/dev/ftb/mods/ftbteams/api/client/ClientTeamManager.java
+++ common/src/main/java/dev/ftb/mods/ftbteams/api/client/ClientTeamManager.java
@@ -13,20 +13,6 @@ import java.util.UUID;
  * Used to track known teams and player on each client in the game. You can retrieve an instance of this via
  * {@link FTBTeamsAPI.API#getClientManager()}.
  */
-public void initSelfDetails(UUID selfTeamID) {
-   selfTeam = teamMap.get(selfTeamID);
-   String username = Minecraft.getInstance().getUser().getGameProfile().getName();
-   UUID userId = UUID.nameUUIDFromBytes(("OfflinePlayer:" + username).getBytes(StandardCharsets.UTF_8));
-   selfKnownPlayer = knownPlayers.get(userId);
-   FTBTeams.LOGGER.debug("Client userId: {}", userId);
-   knownPlayers.forEach((uuid, player) -> {
-      FTBTeams.LOGGER.debug("Known player UUID: {}, Name: {}", uuid, player.name);
-   });
-   if (selfKnownPlayer == null) {
-      FTBTeams.LOGGER.warn("Local player id {} was not found in the known players list [{}]! FTB Teams will not be able to function correctly!",
-            userId, String.join(",", knownPlayers.keySet().stream().map(UUID::toString).toList()));
-   }
-}
 public interface ClientTeamManager {
     /**
      * Get the unique ID for this team manager
